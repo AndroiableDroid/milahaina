e3072ced02cd18e8ebc769400b94c0d8c1bf7cb6
Pooja Kumari <kumarip@codeaurora.org>
2019-05-14 13:23:16 +0530

    milahaina: Build IPACM (IP Catalogue and IP Management).
     * Normally I would just pick up the split makefile at
       vendor/qcom/opensource/data-ipa-cfg-mgr however I cannot
       due to inclusion order in LOS so building them in tree
       will have to suffice.
    
    Change-Id: I6b73948ed0658f512688fe6bf66a1b029f40390a
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Data
 $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_product.mk)
 
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
+
 # Display
 $(call inherit-product, hardware/qcom-caf/sm8350/display/config/display-product.mk)
 $(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-interfaces-product.mk)
@@ -149,6 +153,11 @@ PRODUCT_PACKAGES += \
 # Fastboot
 PRODUCT_PACKAGES += \
     fastbootd
+
+# HIDL
+PRODUCT_PACKAGES += \
+    libhidltransport.vendor \
+    libhwbinder.vendor
 
 # Initialization
 PRODUCT_COPY_FILES += \
