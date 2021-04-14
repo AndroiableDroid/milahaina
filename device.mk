e08c3781d9faebfb28be4efe2a594523daa34fff
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:30:48 +0200

    milahaina: vibrator: Implement OOS style effects for LED vibrator.
     * This change implements the following effects:
       - Effect::CLICK
       - Effect::DOUBLE_CLICK
       - Effect::TICK
       - Effect::HEAVY_CLICK
    
     * Effect write streams were dumped using following strace command:
       - strace -fp `pidof vendor.oneplus.hardware.lmvibrator@1.0-service` -e openat,write
    
     * Also, these effects can easily be tested with following test app:
       - https://github.com/luk1337/VibeTest
    
    Change-Id: Ie65caa0c65dc81bb35a7bcc56870649e895ced3f

--- device.mk
+++ device.mk
@@ -173,6 +173,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.default \
     init.xiaomi.camera.rc \
+    init.xiaomi.haptics.rc \
     init.xiaomi.recovery.rc \
     init.class_main.sh \
     init.qcom.class_core.sh \
