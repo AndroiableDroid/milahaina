43d078f831bd9f30f1be364d4d88cc7cd713bd7a
Alexander Koskovich <zvnexus@outlook.com>
2021-08-19 23:03:56 -0700

    milahaina: init: Add permissions for RCV and speaker calibration.
    Change-Id: I3dcb1e173036403a0c8f44f0fbd468014a4629d6
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -181,6 +181,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     fstab.default \
+    init.xiaomi.audio.rc \
     init.xiaomi.camera.rc \
     init.xiaomi.haptics.rc \
     init.xiaomi.recovery.rc \
