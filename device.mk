ZVNexus/android_device_asus_sake

sake: Move proprietary vendor inclusion to top of makefile.
 * We need any copy-outs here to win over inherited makefiles,
   e.g. display HAL was copying out snapdragon_color_libs_config.xml
   but we replace it with the PXLW one.

Change-Id: I3ef2a8d90a4217bb8424438644d1347556a4284b

device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from our proprietary files directory.
+$(call inherit-product, vendor/asus/sake/sake-vendor.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
@@ -363,9 +366,6 @@ PRODUCT_PACKAGES += \
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager
-
-# Vendor
-$(call inherit-product, vendor/asus/sake/sake-vendor.mk)
 
 # Verified Boot
 PRODUCT_COPY_FILES += \

AndroiableDroid/milahaina

device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from our proprietary files directory.
+$(call inherit-product, vendor/xiaomi/milahaina/milahaina-vendor.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
@@ -360,9 +363,6 @@ PRODUCT_PACKAGES += \
 # Vendor Service Manager
 PRODUCT_PACKAGES += \
     vndservicemanager
-
-# Vendor
-$(call inherit-product, vendor/xiaomi/milahaina/milahaina-vendor.mk)
 
 # Verified Boot
 PRODUCT_COPY_FILES += \