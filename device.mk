b1581311438fad8820de90487e76f37e6fa7a327
Vishalcj17 <vishalcj@aospa.co>
2022-01-23 05:54:15 +0900

    milahaina: Switch to custom 2.0 sensor multihal
    Change-Id: I9ef79f6f4d3ef7a5770b91389b1fd76147336f93
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -512,7 +512,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.hardware.sensors@2.0-service.milahaina-multihal \
     libsensorndkbridge
 
 # Service Tracker
