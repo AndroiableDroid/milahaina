7b954b8cbfb0e4d380428502b2882c4b09e44813
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    milahaina: Update to Boot Control HAL v1.2
    Change-Id: I26da6b732fdce8c69c4390effe3d7b727d71adac
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -125,9 +125,9 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Boot Control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service \
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service \
     bootctrl.lahaina \
     bootctrl.lahaina.recovery
 
