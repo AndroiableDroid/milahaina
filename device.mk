077c3c3db67c97b0cea9e5087dfb75c9b28c3f35
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 17:01:22 +0200

    milahaina: Go away Megvii
    * Nuke Megvii proprietary files and build stub ones from hardware/xiaomi
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- device.mk
+++ device.mk
@@ -156,6 +156,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    libMegviiFacepp-0.5.2 \
+    libmegface \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
