9139498470617fc260630c54ea161ffdf4ed2f3b
Mohd Faraz <androiabledroid@gmail.com>
2022-07-21 16:52:39 +0200

    milahaina: update telephony props & radio libs
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -379,9 +379,32 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.6 \
     android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3 \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0 \
+    android.hardware.radio.deprecated@1.0.vendor \
+    android.system.net.netd@1.1
+
+# Properties
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    DEVICE_PROVISIONED=1 \
+    net.tethering.noprovisioning=true \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    persist.vendor.cne.feature=1 \
+    persist.vendor.data.mode=concurrent \
+    persist.vendor.dpm.feature=11 \
+    persist.vendor.dpm.idletimer.mode=default \
+    ril.subscription.types=NV,RUIM \
+    ro.telephony.default_network=33,33 \
+    ro.telephony.sim_slots.count=2 \
+    ro.vendor.use_data_netmgrd=true \
+    telephony.active_modems.max_count=2 \
+    telephony.lteOnCdmaDevice=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
 
 PRODUCT_VENDOR_PROPERTIES += \
     persist.radio.multisim.config=dsds \
