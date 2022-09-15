a4aed3558bd727cc7b954a768cfaa8bd1fa62f09
Mohd Faraz <androiabledroid@gmail.com>
2022-09-15 15:29:27 +0200

    milahaina: init: Get build description from build fingerprint ** reference commit https://github.com/LineageOS/android_device_xiaomi_sm8250-common/commit/b7290d85035ad11db1335fec2113626b2f792522

--- init/init_milahaina.cpp
+++ init/init_milahaina.cpp
@@ -42,6 +42,39 @@
 using android::base::GetProperty;
 using std::string;
 
+#define FIND_AND_REMOVE(s, delimiter, variable_name) \
+    std::string variable_name = s.substr(0, s.find(delimiter)); \
+    s.erase(0, s.find(delimiter) + delimiter.length());
+
+#define APPEND_STRING(s, to_append) \
+    s.append(" "); \
+    s.append(to_append);
+
+string fingerprint = GetProperty("ro.vendor.build.fingerprint", "");
+
+string fingerprint_to_description(string fingerprint) {
+    string delimiter = "/";
+    string delimiter2 = "/";
+    string build_fingerprint_copy = fingerprint;
+
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, brand)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, product)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter2, device)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, platform_version)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, build_id)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter2, build_number)
+    FIND_AND_REMOVE(build_fingerprint_copy, delimiter, build_variant)
+    std::string build_version_tags = build_fingerprint_copy;
+
+    std::string description = product + "-" + build_variant;
+    APPEND_STRING(description, platform_version)
+    APPEND_STRING(description, build_id)
+    APPEND_STRING(description, build_number)
+    APPEND_STRING(description, build_version_tags)
+
+    return description;
+}
+
 void property_override(char const prop[], char const value[])
 {
     auto pi = (prop_info*) __system_property_find(prop);
@@ -117,6 +150,7 @@ void haydn_vendor_properties()
 void set_build_fingerprint()
 {
     property_override("ro.build.fingerprint", GetProperty("ro.vendor.build.fingerprint", "").c_str());
+    property_override("ro.build.description", fingerprint_to_description(GetProperty("ro.vendor.build.fingerprint", "")).c_str());
 }
 
 void vendor_load_properties()
