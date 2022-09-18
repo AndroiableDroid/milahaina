b1b1a991a6e587720ae89e7146ed2760393861f3
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-09-18 12:22:47 +0200

    Merge commit 'd8795c4f580fdfb8101ce7b64405b4394f2c0f8d' as 'thermal'

--- device.mk
+++ device.mk
@@ -582,7 +582,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.milahaina
 
 # USB
 TARGET_HAS_DIAG_ROUTER := true
