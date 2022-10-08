7d29f5b2835cc4520177c392929c8d898eb860bc
Mohd Faraz <androiabledroid@gmail.com>
2022-10-08 15:05:40 +0200

    milahaina: use source build nqnfcinfo
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -338,6 +338,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_milahaina/android.hardware.nfc.uicc.xml
 
 PRODUCT_PACKAGES += \
+    nqnfcinfo \
     SecureElement \
     Tag \
     NQNfcNci \
